3a30ac81cc6a7cd2f175c63ae79eedaba17ddb48
andrefangeloni <andrefangeloni@gmail.com>
2019-08-06 11:26:17 -0300

    LikeController com match de Likes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
 const routes = express.Router();
 
 routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
 
 module.exports = routes;
