3e6e2c85981a41bffa55e8ab52f7d0278a8f884d
andrefangeloni <andrefangeloni@gmail.com>
2019-08-06 11:03:12 -0300

    DevController criado e já salvando no MongoDB

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
 const express = require("express");
-
+const DevController = require("./controllers/DevController");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.send(`Hello ${req.query.name}`);
-});
-
-routes.post("/devs", (req, res) => {
-  return res.json(req.body);
-});
+routes.post("/devs", DevController.store);
 
 module.exports = routes;
