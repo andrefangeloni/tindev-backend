cd943b9b9136fe3e184e6cc01e6f9848eed06740
andrefangeloni <andrefangeloni@gmail.com>
2019-08-11 22:15:33 -0300

    Socket-io configurado

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -13,7 +13,14 @@ module.exports = {
     }
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log("DEU MATCH!!!");
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+      } else {
+        req.io.to(targetSocket).emit("match", loggedDev);
+      }
     }
 
     loggedDev.likes.push(targetDev._id);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   "mongodb+srv://dba:dba1234@cluster0-vomcv.mongodb.net/omnistack8?retryWrites=true&w=majority",
@@ -11,7 +21,14 @@ mongoose.connect(
   }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 server.listen(3333);
